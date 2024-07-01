9c103c51406dc5f7a49c820c9294d27b9b790bc0
handywts <soul_daddy@hotmail.com>
2024-07-01 11:24:27 -0700

    Delete button produces class name

--- index.js
+++ index.js
@@ -1,19 +1,50 @@
-const todos = ["todo1"];
+const todos = [];
 
 document.getElementById("submitButton").addEventListener("click", handleClick);
+
+document.addEventListener("click", function(e) {
+    
+    const target = e.target.closest(".delete1");
+
+    if(target) {
+        handleDelete(e);
+        //createList();
+    }
+});
 
 
 function handleClick() {
     
     let userInput = document.getElementById("todoInput").value;
-    todos.push(userInput);
-    document.getElementById("todoInput").value = "";
 
-    let individualItem = "";
-    todos.forEach((item) => {
-        individualItem += "<li>" + item + "</li>";
-        document.getElementById("actual").innerHTML = individualItem;
-     }) 
+    if(userInput != "") {
+        todos.push(userInput);
+        document.getElementById("todoInput").value = "";
 
+        createList();
+    }
     console.log(todos);
+}
+
+function handleDelete(specificTodo) {
+    var name = specificTodo.target.className;
+    todos.splice(0,1);
+    
+    
+    console.log(name);
+    console.log("delete worked");
+    console.log(specificTodo.target);
+    console.log(todos);
+
+}
+
+function createList() {
+    
+    let individualItem = "";
+        todos.forEach((item) => {
+            individualItem += '<li>' + item + '</li> <button class="delete1">delete</button>';
+            document.getElementById("actual").innerHTML = individualItem;
+        
+        });
+    
 }
